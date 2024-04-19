b7127adae0ebd77b7af1d7f1b1791b98d5b68c6c
Risav03 <needleweed03@gmail.com>
2024-04-19 14:09:53 +0530

    abi and contractAdds uploaded

---
+++ utils/contractAdds.js
@@ -0,0 +1,12 @@
+"use client"
+
+export const contractAdds = {
+    Jlema : "0x71D9943cb18d9Cb3605bc63Dc6Ce659eB7a78ced",
+
+    JlemaLegendary : "0xC2F53f1D1c37c543b7A6440a247E4146d2E2C468",
+
+    JlemaSE : "0x5A0513CAd8F30237A48338AAd3c89b03F4a55545",
+
+    CLEANToken : "0xeF2c6201f085E972fbaD4FA08beF4BaB660DAc33",
+
+}
