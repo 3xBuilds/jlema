e11f333165083289c9dab1655a470126cbf9785e
im-still-thinking <royaritra1990rocks@gmail.com>
2024-05-03 20:11:03 +0530

    added legendary

--- utils/mapConverter.js
+++ utils/mapConverter.js
@@ -1,10 +1,17 @@
 const fs = require("fs");
 const path = require("path");
 const nfts = require("./nfts.json");
+const legendary = require("./legendary.json");
 
 const resultMap = new Map();
 
-nfts.forEach((obj) => {
+// nfts.forEach((obj) => {
+//   // Extract the number from the name format
+//   const key = Number(obj.name.split("#")[1]);
+//   resultMap.set(key, obj);
+// });
+
+legendary.forEach((obj) => {
   // Extract the number from the name format
   const key = Number(obj.name.split("#")[1]);
   resultMap.set(key, obj);
@@ -13,6 +20,6 @@ nfts.forEach((obj) => {
 //   console.log(resultMap);
 const newMap = new Map([...resultMap.entries()].sort((a, b) => a[0] - b[0]));
 
-const outputFilePath = "./mapNfts.json";
+const outputFilePath = "./mapLegendary.json";
 fs.writeFileSync(outputFilePath, JSON.stringify(Object.fromEntries(newMap)) , 'utf-8');
 console.log(`Merged data written to file: ${outputFilePath}`);
