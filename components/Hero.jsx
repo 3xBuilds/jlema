b0940ac364962bb39822bd310fe808fee191666a
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-06-16 12:28:39 +0530

    home page responsiveness done

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,17 +6,20 @@ import { WalletConnectButton } from './buttons/walletConnectButton'
 const Hero = () => {
 
   return (
-    <div className='grid grid-cols-2 w-full h-screen '>
-        <div className='flex items-center justify-start h-full'>
+    <div className='grid grid-cols-2 w-full h-screen max-md:flex max-md:flex-col max-md:mt-32'>
+        <div className='flex items-center justify-start h-full max-md:hidden'>
             <Image src={heroImage} className=' w-[80%] my-auto'/>
         </div>
-        <div className='flex items-center justify-center'>
-            <div className=' mr-20'>
-                <h1 className='font-bold text-5xl mb-3'>Join the collectors</h1>
-                <h2 className='font-normal text-jel-gray-4 text-base mb-5 w-[450px]'>Collect badges, customize your profile and earn points. Connect your wallet and begin the Jlema collector profile experience now.</h2>
+        <div className='flex items-center justify-center max-md:justify-start max-md:px-6'>
+            <div className=' mr-20 max-md:m-0'>
+                <h1 className='font-bold text-5xl max-lg:text-3xl max-md:text-5xl mb-3 max-sm:text-4xl'>Join the <br className="md:hidden"/> collectors</h1>
+                <h2 className='font-normal text-jel-gray-4 max-lg:text-sm max-lg:w-[350px] max-md:w-full max-md:px-0 text-base mb-5 w-[450px]'>Collect badges, customize your profile and earn points. Connect your wallet and begin the Jlema collector profile experience now.</h2>
                 {/* <button className='bg-black text-white rounded-xl cursor-pointer py-3 px-6'>Connect wallet</button> */}
                 <WalletConnectButton/>
             </div>
+        </div>
+        <div className='flex items-center justify-center w-screen md:hidden'>
+            <Image src={heroImage} className='w-full my-auto object-top'/>
         </div>
     </div>
   )
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,33 +1,50 @@
 "use client"
+import { useState } from 'react'
 import Image from 'next/image'
 import logo from '../assets/mainLogo.png'
 import { usePathname, useRouter } from 'next/navigation'
+import { IoClose, IoMenuOutline } from "react-icons/io5";
 
 const Navbar = () => {
 
   const path = usePathname();
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className={`w-full px-8 py-3 flex justify-between fixed bg-white z-40 top-0 left-0 ${(path=="/" | path=="/leaderboard") ? "" : " border-b-[1px] border-jel-gray-3"} `}>
-        <div>
-          <Image src={logo} className='w-32'/>
-        </div>
-        <ul className='flex flex-row gap-2 font-semibold items-center'>
-            <li className='px-6 py-2 cursor-pointer hover:bg-jel-gray-2 duration-200 rounded-xl'>Home</li>
-            <li className='px-6 py-2 cursor-pointer hover:bg-jel-gray-2 duration-200 rounded-xl'>Ecosystem</li>
-            <li className='px-6 py-2 cursor-pointer hover:bg-jel-gray-2 duration-200 rounded-xl'>Community</li>
-            <li className='px-6 py-2 cursor-pointer hover:bg-jel-gray-2 duration-200 rounded-xl'>Info</li>
-            <a href="https://app.komet.me/nfts/Jlema/415" target='_blank'><li className='px-6 py-2 bg-black text-white cursor-pointer hover:bg-jel-gray-2 duration-200 rounded-xl'>Buy</li></a>
-            {/* <li onClick={()=>{
-              router.push('/profile');
-            }} className='px-3 py-3 cursor-pointer hover:bg-jel-gray-2 duration-200 rounded-lg bg-secondary'>
-                <svg width="16" height="18" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M17 19C17 17.6044 17 16.9067 16.8278 16.3389C16.44 15.0605 15.4395 14.06 14.1611 13.6722C13.5933 13.5 12.8956 13.5 11.5 13.5H6.5C5.10444 13.5 4.40665 13.5 3.83886 13.6722C2.56045 14.06 1.56004 15.0605 1.17224 16.3389C1 16.9067 1 17.6044 1 19M13.5 5.5C13.5 7.98528 11.4853 10 9 10C6.51472 10 4.5 7.98528 4.5 5.5C4.5 3.01472 6.51472 1 9 1C11.4853 1 13.5 3.01472 13.5 5.5Z" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-            </li> */}
-        </ul>
-    </div>
+    <>
+      <div className={`w-full px-8 max-md:px-4 py-3 flex justify-between fixed bg-white z-40 top-0 left-0 ${(path=="/" | path=="/leaderboard") ? "" : " border-b-[1px] border-jel-gray-3"} `}>
+          <div className="max-lg:flex flex-row items-center justify-center gap-2 z-50">
+            <div className="lg:hidden" 
+            onClick={()=>setIsOpen(prev=>!prev)}
+            >
+              { isOpen ? <IoClose className="text-3xl"/> : <IoMenuOutline className="text-3xl"/>}
+            </div>
+            <Image src={logo} className='w-32 max-lg:w-28'/>
+          </div>
+          <ul className='flex flex-row gap-2 font-semibold items-center max-md:hidden'>
+              <li className='px-6 py-2 cursor-pointer hover:bg-jel-gray-2 duration-200 rounded-xl'>Home</li>
+              <li className='px-6 py-2 cursor-pointer hover:bg-jel-gray-2 duration-200 rounded-xl'>Ecosystem</li>
+              <li className='px-6 py-2 cursor-pointer hover:bg-jel-gray-2 duration-200 rounded-xl'>Community</li>
+              <li className='px-6 py-2 cursor-pointer hover:bg-jel-gray-2 duration-200 rounded-xl'>Info</li>
+              <a href="https://app.komet.me/nfts/Jlema/415" target='_blank'><li className='px-6 py-2 bg-black text-white cursor-pointer hover:bg-jel-gray-2 duration-200 rounded-xl'>Buy</li></a>
+          </ul>
+          <h3 className=' py-2 text-sm font-semibold lg:hidden relative z-50'>Buy now</h3>
+          {/* Opens on clicking hamburger and shows only for phones */}
+          <div className={`w-screen z-40 shadow-lg shadow-black/50 -translate-y-80 rounded-xl bg-white pt-20 pb-5 fixed top-0 left-0 flex flex-col items-start px-5 justify-center gap-4 duration-300 transition-all ${isOpen ? "translate-y-0" : ""}`}>
+            <div className="flex flex-col items-start gap-4">
+              <div className='flex flex-col gap-4'>
+                <h3 onClick={()=>{setIsOpen(prev=>!prev)}} className='text-base font-medium cursor-pointer'>Home</h3>
+                <h3 onClick={()=>{setIsOpen(prev=>!prev)}} className='text-base font-medium cursor-pointer'>Ecosystem</h3>
+                <h3 onClick={()=>{setIsOpen(prev=>!prev)}} className='text-base font-medium cursor-pointer'>Community</h3>
+                <h3 onClick={()=>{setIsOpen(prev=>!prev)}} className='text-base font-medium cursor-pointer'>Info</h3>
+              </div>
+            </div>
+          </div>
+      </div>
+      
+    </>
+
   )
 }
 
