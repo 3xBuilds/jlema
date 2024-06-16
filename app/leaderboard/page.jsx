183550ff7d9a889ed27b794216153911b8f948d7
SayakSarkar02 <sayaksarkar73@gmail.com>
2024-06-16 14:33:50 +0530

    leaderboard footer added

--- app/leaderboard/page.jsx
+++ app/leaderboard/page.jsx
@@ -8,6 +8,8 @@ import image from "@/assets/icons/imagebadge.png";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { IoLogoDiscord, IoLogoTwitter } from "react-icons/io5";
+import { FaDiscord } from "react-icons/fa";
 
 const Leaderboard = () => {
 
@@ -130,6 +132,13 @@ const Leaderboard = () => {
         
         </div>
       </div>
+      <div className="flex flex-row max-md:flex-col max-md:items-start max-md:justify-start max-md:px-0 items-center justify-between px-10 border-t-[1px] border-jel-gray-3 py-5 mt-10">
+        <h3 className="text-sm text-black">©2024 Jlema. All Rights Reserved.</h3>
+        <div className="flex flex-row items-center gap-8 max-md:mt-2">
+          <IoLogoTwitter className=" text-xl"/>
+          <FaDiscord className=" text-xl"/>
+        </div>
+      </div> 
     </div>
   );
 };
