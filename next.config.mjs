fada14ae601e0e05aa8c0324eb59f68338deefd4
Risav03 <needleweed03@gmail.com>
2024-07-01 21:28:33 +0530

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        domains: ['jlema-raffle-storage.s3.amazonaws.com'],
         unoptimized: true,
         remotePatterns: [
             {
