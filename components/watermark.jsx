23397782059c249558f28da810f265569de95a86
Risav03 <needleweed03@gmail.com>
2024-07-03 14:36:49 +0530

    watermark re colour

--- components/watermark.jsx
+++ components/watermark.jsx
@@ -13,7 +13,7 @@ return (
  className='fixed bottom-0 right-0 z-[99999999]'>
     <div className='bg-black/60 backdrop-blur-sm px-4 py-1.5 rounded-tl-xl text-white text-xs p-1 flex flex-row gap-4 shadow-xl sha shadow-[#000000] translate-x-[124px] duration-300 hover:translate-x-0 cursor-pointer select-none border-[#000000] font-bold border-t-[1px] items-center justify-center'>
         <Image src={logo} className='w-4 brightness-90 '/>
-        <h3 className={`${montserrat.className} text-zinc-500 `}>Built by <span className=' text-[#ffffff]'>3xBuilds</span></h3>
+        <h3 className={`${montserrat.className} text-zinc-300 `}>Built by <span className=' text-[#ffffff]'>3xBuilds</span></h3>
     </div>
 </a>
 )
