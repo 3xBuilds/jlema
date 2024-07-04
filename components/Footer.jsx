320abe1994ddab866117abd90fa33788f7862a33
Risav03 <needleweed03@gmail.com>
2024-07-04 11:14:32 +0530

    discord and twitter added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { IoLogoTwitter } from "react-icons/io5";
+import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaDiscord } from "react-icons/fa";
 
 const Footer = () => {
@@ -8,8 +8,8 @@ const Footer = () => {
         <div className="flex flex-row max-md:flex-col max-md:items-start max-md:justify-start max-md:px-0 items-center justify-between px-10 border-t-[1px] border-jel-gray-3 py-5 mt-10">
         <h3 className="text-sm text-black">©2024 Jlema. All Rights Reserved.</h3>
         <div className="flex flex-row items-center gap-8 max-md:mt-2">
-          <IoLogoTwitter className=" text-xl"/>
-          <FaDiscord className=" text-xl"/>
+          <a href="https://x.com/JlemaNFT" target='_blank'><FaSquareXTwitter className=" text-xl"/></a>
+          <a href="https://discord.gg/Kp9WdpUh3a" target='_blank'><FaDiscord className=" text-xl"/></a>
         </div>
       </div> 
     </div>
--- components/watermark.jsx
+++ components/watermark.jsx
@@ -10,8 +10,8 @@ return (
 <a href='https://www.3xbuilds.com/'
     target='_blank'
     rel='noopener noreferrer' 
- className='fixed bottom-0 right-0 z-[99999999]'>
-    <div className='bg-black/60 backdrop-blur-sm px-4 py-1.5 rounded-tl-xl text-white text-xs p-1 flex flex-row gap-4 shadow-xl sha shadow-[#000000] translate-x-[124px] duration-300 hover:translate-x-0 cursor-pointer select-none border-[#000000] font-bold border-t-[1px] items-center justify-center'>
+ className='fixed bottom-0 right-0 z-[99999999] translate-x-[124px] duration-300 hover:translate-x-0'>
+    <div className='bg-black/60 backdrop-blur-sm px-4 py-1.5 rounded-tl-xl text-white text-xs p-1 flex flex-row gap-4 shadow-xl  shadow-[#000000] cursor-pointer select-none border-[#000000] font-bold border-t-[1px] items-center justify-center'>
         <Image src={logo} className='w-4 brightness-90 '/>
         <h3 className={`${montserrat.className} text-zinc-300 `}>Built by <span className=' text-[#ffffff]'>3xBuilds</span></h3>
     </div>
