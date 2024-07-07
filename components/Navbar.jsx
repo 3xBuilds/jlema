6ffbfb88bf4c13aa8b060f6862358dc25679723d
Risav03 <needleweed03@gmail.com>
2024-07-07 23:14:08 +0530

    ok

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
             <div className={`w-screen z-40 shadow-lg shadow-black/50 -translate-y-96 rounded-xl bg-white pt-20 pb-5 fixed top-0 left-0 flex flex-col items-start px-5 justify-center gap-4 duration-300 transition-all ${isOpen ? "translate-y-1" : ""}`}>
               <div className="flex flex-col items-start w-full gap-4">
                 <div className='flex flex-col w-full gap-4'>
-                  <h3 onClick={()=>{setIsOpen(prev=>!prev)}} className=' px-2 py-2 text-base font-medium cursor-pointer'>Home</h3>
+                  <h3 onClick={()=>{setIsOpen(prev=>!prev)}} className=' px-2 py-2 text-base font-medium cursor-pointer'><a href='https://jlema.xyz' target='_blank' >Home</a></h3>
                 <h3 className=' px-2 py-2 cursor-pointer hover:bg-jel-gray-2 duration-200 w-full'><button onClick={()=>{router.push("/leaderboard")}} className='w-full text-start'>Leaderboard</button></h3>
                   <h3 onClick={()=>{setIsOpen(prev=>!prev)}} className=' px-2 py-2 text-base font-medium cursor-pointer'>Ecosystem</h3>
                   <h3 onClick={()=>{setIsOpen(prev=>!prev)}} className=' px-2 py-2 text-base font-medium cursor-pointer'>Community</h3>
